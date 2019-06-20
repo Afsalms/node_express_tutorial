40b27142d121d5403326d172c2a63b07ee5e37bb
afsals <afsals@qburst.com>
2019-06-20 16:42:10 +0530

    router

--- router/main.js
+++ router/main.js
@@ -1,5 +1,12 @@
 
+function test(){
+    console.log("test fucntion")
+    // next()
+}
+
 module.exports = function(app){
+
+
 
     app.get('/',function(req,res){
         res.render('index.html')
@@ -8,4 +15,25 @@ module.exports = function(app){
     app.get('/about',function(req,res){
         res.render('about.html');
     });
+
+    app.get('/example', function (req, res, next) {
+        console.log('the response will be sent by the next function ...')
+        next()
+    }, function (req, res) {
+        res.send('Hello from B!')
+    })
+
+    app.get('/example2', function (req, res, next) {
+        console.log('the response will be sent by the next function ...')
+        next()
+    });
+
+    app.get('/user/:id/', function(req, res){
+        console.log(req.params)
+        console.log(req.query)
+        res.send("succcess")
+    })
+
+    // app.use(test)
+
 }
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 
-require('./router/main')(app);
+// require('./router/main')(app);
+require('./router/route2')
+
+var birds = require('./router/route2')
+app.use('/birds', birds)
+ 
+
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
